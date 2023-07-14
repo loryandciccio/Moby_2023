b6c4cdcbbba0b4dd217b73d43bc62d8a758bfa67
luc-hub <luc.sparapani@stud.uniroma3.it>
2023-07-14 16:00:30 +0200

    commit-23-levelmenuAggiornato

--- Mobile_computing_2023/Assets/Scripts/GameManager.cs
+++ Mobile_computing_2023/Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteAll();
+        
         gestioneScene = FindObjectOfType<GestioneScene>();
         ScriviValoreMonete();
         ScriviValoreMosche();
