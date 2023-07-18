9f89f7c131dc8b4388958f060d016de6c3654d5a
uchihaandrea <127963323+KatoteshiKuka@users.noreply.github.com>
2023-07-18 21:33:59 +0200

    Commit-35_FinallLevelisWin

--- Mobile_computing_2023/Assets/Iguana.cs
+++ Mobile_computing_2023/Assets/Iguana.cs
@@ -8,10 +8,17 @@ public class Iguana : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Newbie") && PlayerPrefs.GetInt("numeroMosche") == 10){
+        if (collision.CompareTag("Newbie") && PlayerPrefs.GetInt("numeroMosche") == 10 && SceneManager.GetActiveScene().name != "level3")
+        {
             UnlockNewLevel();
             SceneManager.LoadScene("level" + (SceneManager.GetActiveScene().buildIndex + 1));
             PlayerPrefs.SetInt("numeroMosche",0);
+        }
+        if (collision.CompareTag("Newbie") && PlayerPrefs.GetInt("numeroMosche") == 10 && SceneManager.GetActiveScene().name == "level3")
+        {
+            
+            SceneManager.LoadScene("win");
+            PlayerPrefs.SetInt("numeroMosche", 0);
         }
 
     }
